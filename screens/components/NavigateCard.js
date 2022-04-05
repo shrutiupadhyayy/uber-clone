be2232ea7ff15b649f4c41d0badb1e76a4f2d478
Shruti <shrutii.5599@gmail.com>
2022-04-05 17:15:56 +0530

    navCard

--- screens/components/NavigateCard.js
+++ screens/components/NavigateCard.js
@@ -3,8 +3,15 @@ import React from 'react'
 import tw from 'tailwind-react-native-classnames'
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete"
 import { GOOGLE_MAPS_APIKEY } from "@env"
+import { useDispatch } from 'react-redux'
+import { setDestination } from "../../slices/navSlice"
+import { useNavigation } from '@react-navigation/native'
+
 
 const NavigateCard = () => {
+  const  dispatch = useDispatch();
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
       <Text style={tw`text-center py-5 text-xl`}> BAM BAM AYYY</Text>
@@ -12,6 +19,23 @@ const NavigateCard = () => {
         <GooglePlacesAutocomplete 
         placeholder='Where to?'
         styles={toInputBoxStyles}
+        fetchDetails={true}
+        returnKeyType={"search"}
+        minLength={2}
+        onPress={(data, details = null ) => {
+            dispatch(
+              setDestination({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            )
+              navigation.navigate('RideOptionsCard')
+        }}
+        enablePoweredByContainer={false}
+        query={{
+          key: GOOGLE_MAPS_APIKEY,
+          language: "en",
+        }}
         nearbyPlacesAPI='GooglePlacesSearch'
         debounce={400}
         />
